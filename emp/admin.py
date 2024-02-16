a7b78e78bbcc18b422e92e568d24566eaf79c7b5
nikhilsahu2002 <nikhil1233kumar@gmail.com>
2024-02-17 00:07:36 +0530

    commit

--- emp/admin.py
+++ emp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from emp.models import Company,Employee
 
 # Register your models here.
+
+
+class CoampnayAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display =['name']
+    
+
+admin.site.register(Company,CoampnayAdmin)
+admin.site.register(Employee,EmployeeAdmin)
+
--- emp/views.py
+++ emp/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework import viewsets 
 from emp.models import Company,Employee
 from emp.serializers import CompanySerializer,EmployeeSerializer
-
+from rest_framework.decorators import action 
+from rest_framework.response import Response
 # Create your views here.
 class ComanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
+    # company/{compnay_id}/employee
+    @action(detail=True,methods=['get'])
+    def employee(self,req,pk):
+       try:
+            company =Company.objects.get(pk=pk)
+            emps = Employee.objects.filter(company = company)
+            empsrilizer = EmployeeSerializer(emps,many =True,context ={'request':req})
+            return Response(empsrilizer.data)
+       except Exception as e:
+           return Response({'message':'error the dataset is empty'})
+           
+
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+    serializer_class = EmployeeSerializer()
